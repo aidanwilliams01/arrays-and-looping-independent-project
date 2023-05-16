f27bcabe81108321026cdfaa7e04fd887544800a
Aidan Williams <aidanwilliams01@gmail.com>
2023-05-16 15:52:07 -0700

    update beepBoop function to return a string with all integers from 0 to the input number

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Describe: beepBoop()
 Test: "It should return a string with a 0 if the number 0 is inputted"
 Code: beepBoop(0);
 Expected Output: '0'
+
+Test: "It should return a string with all integers from 0 to the input number"
+Code: beepBoop(9);
+Expected Output: '0, 1, 2, 3, 4, 5, 6, 7, 8, 9'
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 function beepBoop(inputNumber) {
-  const stringInputNumber = inputNumber.toString();
-  return stringInputNumber;
+  let outputString = '';
+  for (let index = 0; index <= inputNumber; index++) {
+    if (index === 0) {
+      outputString = '0';
+    }
+    else {
+      outputString = `${outputString}, ${index}`;
+    }
+  }
+  return outputString;
 }
