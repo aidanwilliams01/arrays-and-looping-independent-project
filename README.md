2a93b59aace11160c840201a1f2673573cbf0932
Aidan Williams <aidanwilliams01@gmail.com>
2023-05-17 14:29:39 -0700

    add beepBoopReverse function

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ Expected Output: '0, Beep!, 2, 3, 4, 5, 6, 7, 8, 9'
 Test: "It should return a string with all integers containing 2 (except for those containing 3) replaced with 'Boop!' and all integers containing 3 replaced with 'Won't you be my neighbor?'"
 Code: beepBoop(9);
 Expected Output: '0, Beep!, Boop!, Won't you be my neighbor?, 4, 5, 6, 7, 8, 9'
+
+Describe: beepBoopReverse()
+
+Test: "It should follow the logic of beepBoop, but the returned string should be in reverse order."
+Code: beepBoop(30);
+Expected Output: 'Won't you be my neightbor?, 29, 28, 27...Won't you be my neighbor?, Boop!, Beep!, 0'
--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,36 @@ function beepBoop(inputNumber) {
   }
   return outputString;
 }
+
+function beepBoopReverse(inputNumber) {
+  let outputString = '';
+  for (let index = inputNumber; index >= 0; index--) {
+    if (index === inputNumber) {
+      if (index.toString().includes('3')) {
+        outputString = `Won't you be my neighbor?`;
+      }
+      else if (index.toString().includes('2')) {
+        outputString = `Boop!`;
+      }
+      else if (index.toString().includes('1')) {
+        outputString = `Beep!`;
+      }
+      else {
+        outputString = `${index}`;
+      }
+    }
+    else if (index.toString().includes('3')) {
+      outputString = `${outputString}, Won't you be my neighbor?`;
+    }
+    else if (index.toString().includes('2')) {
+      outputString = `${outputString}, Boop!`;
+    }
+    else if (index.toString().includes('1')) {
+      outputString = `${outputString}, Beep!`;
+    }
+    else {
+      outputString = `${outputString}, ${index}`;
+    }
+  }
+  return outputString;
+}
